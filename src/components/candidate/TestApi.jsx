9c4aedfd094e73ee74fe0102b2ffe7e08b6e6ce1
Shen0003 <yongshen8@gmail.com>
2025-06-20 14:20:43 +0800

    updated

--- src/components/candidate/TestApi.jsx
+++ src/components/candidate/TestApi.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const BACKEND_URL = "https://adkbev2.onrender.com/"; // deployed FastAPI backend
+const BACKEND_URL = "https://adkbev2.onrender.com"; // deployed FastAPI backend
 
 const TestAPI = () => {
   const navigate = useNavigate();
